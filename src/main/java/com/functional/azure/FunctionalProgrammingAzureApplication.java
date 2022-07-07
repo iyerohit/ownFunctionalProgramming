7bb08fea45061118bf8381fa57c1d19aa685fd9a
iyerohit <46613692+iyerohit@users.noreply.github.com>
2022-07-07 17:20:14 +0530

    Update FunctionalProgrammingAzureApplication.java

--- src/main/java/com/functional/azure/FunctionalProgrammingAzureApplication.java
+++ src/main/java/com/functional/azure/FunctionalProgrammingAzureApplication.java
@@ -3,11 +3,20 @@ package com.functional.azure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class FunctionalProgrammingAzureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunctionalProgrammingAzureApplication.class, args);
 	}
 
+
+	@RequestMapping("/")
+    public String getNormal() {
+
+        return "YOu are calling default function in java deployed in azure";
+    }
 }
